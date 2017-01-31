4ad7e4aeb77624d24e67cb8fa60f7c5f3a16d97a
skhamoud <skhamoud@gmail.com>
2017-01-31 01:59:15 +0000

    Style weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -9,28 +9,28 @@ const icons = _.icons;
 
 function Weather(props) {
   return (
-    <div className="weather-container">
+    <div className="weather">
       <h2>{props.city} </h2>
-      <div>
+      <div className="wc wc__status" >
         <Icon weatherCode={props.weatherCode} title={props.weatherDescription}/>
       </div>  
-      <div>
+      <div  className="wc">
           <Icon className={icons.temperature} title="Temperature"/>
-          {props.temperature} &deg;C
+          <span className="wc__temp" >{props.temperature} &deg; C</span>
       </div>
-      <span>{_.showTime()}</span>
-      <div className="weather-row">
+      <div  className="wc">{_.showTime()}</div>
+      <div className="wc weather__row">
         <span>
           <Icon className={icons.wind} title="Wind"/>
-          {props.windSpeed} m/s
+          <br/>{props.windSpeed} m/s
         </span>
         <span>
           <Icon className={icons.humidity} title="humidity" />
-          {props.humidity}%
+          <br/>{props.humidity} %
         </span>
         <span>
           <Icon className={icons.pressure} title="pressure" /> 
-          {Number(props.pressure).toFixed(0)} hpa
+          <br/>{Number(props.pressure).toFixed(0)} hpa
         </span>
       </div>
 
