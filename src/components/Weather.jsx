939dd2f65a0f9703156f05074c6860696cea24d7
skhamoud <skhamoud@gmail.com>
2017-01-31 01:15:35 +0000

    implement weather component

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,18 +1,39 @@
 import React from "react";
-import Icon from "./Icon";
+import _ from "../helpers";
+import Icon from "./icon";
 // ============================================
-import "./css/Weather.css";
+import "../sass/weather.scss";
+
+/* Icons Class Names from Weather-icons  */
+const icons = _.icons;
 
 function Weather(props) {
   return (
-    <div>
-      <h2>City: {props.city} </h2>
-      <p>Weather: {props.weather.main}</p>
-      <p>Temperature : {Number(props.main.temp).toFixed(1)} &deg;C</p>
-      <p>Weather Description:
-        <Icon icon={props.weather.icon}/>
-        {props.weather.description}
-      </p>
+    <div className="weather-container">
+      <h2>{props.city} </h2>
+      <div>
+        <Icon weatherCode={props.weatherCode} title={props.weatherDescription}/>
+      </div>  
+      <div>
+          <Icon className={icons.temperature} title="Temperature"/>
+          {props.temperature} &deg;C
+      </div>
+      <span>{_.showTime()}</span>
+      <div className="weather-row">
+        <span>
+          <Icon className={icons.wind} title="Wind"/>
+          {props.windSpeed} m/s
+        </span>
+        <span>
+          <Icon className={icons.humidity} title="humidity" />
+          {props.humidity}%
+        </span>
+        <span>
+          <Icon className={icons.pressure} title="pressure" /> 
+          {Number(props.pressure).toFixed(0)} hpa
+        </span>
+      </div>
+
     </div>
   );
 }
